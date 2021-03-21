1b42d625c4ada4778c22f65124629aab6f8a5141
phuong98-sys <phuongred98@gmail.com>
2021-03-22 02:21:39 +0700

    22/3- sang

---
+++ MISA.CukCuk/Misa.ApplicationCore/Model/BaseEntity.cs
@@ -0,0 +1,29 @@
+﻿using Misa.ApplicationCore.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Misa.ApplicationCore.Model
+{
+   public  class BaseEntity
+    {
+        public EntityState EntityState { get; set; } = EntityState.AddNew;
+        /// <summary>
+        /// Ngày tạo
+        /// </summary>
+        public DateTime? CreatedDate { get; set; }
+        /// <summary>
+        /// Người tạo
+        /// </summary>
+        public string CreatedBy { get; set; }
+        /// <summary>
+        /// Ngày sửa
+        /// </summary>
+        public DateTime? ModifiedDate { get; set; }
+        /// <summary>
+        /// Người sửa
+        /// </summary>
+        public string ModifiedBy { get; set; }
+
+    }
+}
